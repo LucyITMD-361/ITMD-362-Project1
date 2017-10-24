2ef34d8687c7bf210edf8a69c691c4c6042b3d18
LucyITMD-361 <Lgarcia11@hawk.iit.edu>
2017-10-24 16:36:16 -0500

    Added a first name validator

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -8,23 +8,11 @@ function ValidateEmail(mail)
     alert("Invalid email")  
     return (false)  
 }  
-
-function checkForm(){
-	var first_name = document.getElementById("first_name").value;
-	var last_name = document.getElementById("last_name").value;
-	var email_field = document.getElementById("mail").value;
-	var phone_field = document.getElementById("tel").value;
-	if (fName != "" && lName !="" && email != "" && phone != ""){
-		if(mailVal(email) && telVal(phone)) {
-			fff();
-			setTimeout(function(){location.reload(true)}, 1000);
-		}else if(!mailVal(email)){
-			document.getElementById("mail").style.borderColor = "red";
-		}
-	}
-	console.log(fName);
+//firstname validation
+function validateForm() {
+    var x = document.forms["formInfo"]["first_name"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
 }
-
-var form = document.getElementsByTagName('button')[0];
-form.addEventListener("click", function(form){form.preventDefault()});
-form.addEventListener("click", checkForm, false);
